3dbce50638f9166a4dfd41254f97392ba3b56cc9
abanoub <abanoub>
2024-01-13 15:55:35 +0200

    add user and token apis

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -24,5 +24,6 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/scheme/', SpectacularAPIView.as_view(), name='api-scheme'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-scheme'), name='api-docs')
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-scheme'), name='api-docs'),
+    path('api/user/', include('user.urls')),
 ]
